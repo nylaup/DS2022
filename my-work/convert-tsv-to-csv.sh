d5d49d37cf172fa80cecd9bbefd469d01c1d7bcb
Nylaup <nylaupal@gmail.com>
2024-09-17 10:39:14 -0400

    Worked on scripting basics

---
+++ my-work/convert-tsv-to-csv.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#Use tr - translate 
+tr '\t' ',' < file.tsv > file.csv
+
+#Use sed - stream editor
+sed 's/'$'\t''/,/g' file.tsv > file.csv
+
+#Use awk - pattern scanning and processing language
+awk 'BEGIN { FS="\t"; OFS="," } {$1=$1; print}' file.tsv > file.csv
