219eb0c6605f2fad536dc55a287dd311c528f951
Nylaup <nylaupal@gmail.com>
2024-09-21 18:18:08 -0400

    Created madlib file

---
+++ my-work/lab3/madlib.sh
@@ -0,0 +1,25 @@
+
+#!/bin/bash
+
+clear
+echo "Let's build a mad-lib!"
+
+read -p "1. Name an unusual sport: " NOUN1
+read -p "2. Name a relative: " NOUN2
+read -p "3. Name a nongeographical location: " NOUN3
+read -p "4. Name a piece of sports equipment: " NOUN4
+read -p "5. Name a color: " ADVERB1
+read -p "6. Name a verb: " VERB1
+read -p "7. Name an adverb: " ADVERB2
+read -p "8. Name an adjective: " ADJECTIVE1
+
+
+echo "Over the summer, I was at a party and was asked whether I wanted to play $NOUN1."
+echo "I turned to see who asked it, and turns out it was my $NOUN2."
+echo "I said yes, and we went out together to the $NOUN3."
+echo "They handed me a $NOUN4 that was very vibrantly $ADVERB1."
+echo "'What is this game?' I asked, 'How do you even play this?'"
+echo "'Well, you just $VERB1 $ADVERB2' my $NOUN2 answered."
+echo "'This is the most $ADJECTIVE1 sport ever!' I exclaimed."
+
+
