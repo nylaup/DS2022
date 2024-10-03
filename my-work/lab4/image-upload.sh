1efdf58ddd68301bb19440ed191c995e4da8c0f3
Nylaup <nylaupal@gmail.com>
2024-10-03 10:55:22 -0400

    Finished upload image python file

--- my-work/lab4/image-upload.sh
+++ my-work/lab4/image-upload.sh
@@ -6,4 +6,4 @@ aws s3 cp skypic.jpg s3://ds2022-mge9dn/
 aws s3 presign --expires-in 604800 s3://ds2022-mge9dn/skypic.jpg
 
 
-https://ds2022-mge9dn.s3.us-east-1.amazonaws.com/skypic.jpg?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAU5LH5YXYTQNAFRFS%2F20241001%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20241001T212435Z&X-Amz-Expires=604800&X-Amz-SignedHeaders=host&X-Amz-Signature=8feb14b97c32c8a472e619e58a1972aa369498ba72cd2e11ce1ce84213961c88
+#https://ds2022-mge9dn.s3.us-east-1.amazonaws.com/skypic.jpg?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAU5LH5YXYTQNAFRFS%2F20241001%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20241001T212435Z&X-Amz-Expires=604800&X-Amz-SignedHeaders=host&X-Amz-Signature=8feb14b97c32c8a472e619e58a1972aa369498ba72cd2e11ce1ce84213961c88
--- my-work/lab4/upload-image.py
+++ my-work/lab4/upload-image.py
@@ -2,7 +2,22 @@
 #!/usr/bin/python3
 
 import boto3
+import urllib
+s3 = boto3.client('s3', region_name="us-east-1")
 
-s3 = boto3.client('s3', region_name='us-east-1')
+pic_url = "https://www.shutterstock.com/image-photo/rainstorm-over-beautiful-landscape-260nw-167600330.jpg"
+s3_key = "rainimage.jpg"
 
+bucket_name = "ds2022-mge9dn"
+object_name = "rainimage.jpg"
+expires_in = 604800
 
+urllib.request.urlretrieve(pic_url, object_name)
+
+s3.upload_file(object_name, bucket_name, s3_key)
+
+response = s3.generate_presigned_url(
+    'get_object',
+    Params={'Bucket': bucket_name, 'Key': object_name},
+    ExpiresIn=expires_in
+)
